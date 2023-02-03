a14e3d8953bf4b22f2fb557274afcf8d9e0f77b9
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-02-03 12:16:14 -0600

    first real commit

---
+++ orlundo_portfolio/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+      <section className="bg-blue-700 min-h-screen"></section>
+    </main>
+    
+  )
+  }
+  
